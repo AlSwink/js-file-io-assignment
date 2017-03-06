483b22206817c1f07ecf6d58530682384c28c9b2
Unknown <ajswink01@gmail.com>
2017-03-06 14:30:31 -0600

    Finished JavaScript File IO
    Assignment completed

--- Employee.js
+++ Employee.js
@@ -3,11 +3,22 @@
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (n, t, s) {
+    this.name = n
+    this.title = t
+    this.salary = s
   }
 
   // TODO ???
+  static parseFromFilePath (path) {
+    const data = JSON.parse(fs.readFileSync(path, 'utf8'))
+    return new Employee(data.name, data.title, data.salary)
+  }
+
+  promote (title, salary) {
+    this.title = title
+    this.salary = salary
+  }
 }
 
 // TODO ???
